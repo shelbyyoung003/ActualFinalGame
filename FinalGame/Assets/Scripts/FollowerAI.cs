301f02c12eabf1ab11ddd9b7575bb62797610312
Shelby <shelbyyoung003@gmail.com>
2018-11-06 16:43:22 -0700

    Added Echo AI

---
+++ FinalGame/Assets/Scripts/FollowerAI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class FollowerAI : MonoBehaviour {
+
+	private UnityEngine.AI.NavMeshAgent agent;
+	public Transform Destination;
+
+	void Start ()
+	{
+		agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+
+	}
+	
+	void Update ()
+	{
+		if (Destination != null) agent.destination = Destination.position;
+	}
+}
