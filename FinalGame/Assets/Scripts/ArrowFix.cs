90282870392257b1336e5f562e876d6859eea638
Shelby <shelbyyoung003@gmail.com>
2018-12-06 00:12:15 -0700

    While Loop + Arrow Rotation Fix

---
+++ FinalGame/Assets/Scripts/ArrowFix.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowFix : MonoBehaviour
+{
+
+	public Vector3 Movement;
+
+	// Use this for initialization
+	void Start ()
+	{
+		//transform.Translate(Movement);
+		transform.Rotate(Movement);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ FinalGame/Assets/Scripts/Counter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour
+{
+
+	private Text counterLabel;
+	public float seconds = 1.0f;
+	public int numbers = 3;
+
+	// Use this for initialization
+	IEnumerator Start ()
+	{
+
+		counterLabel = GetComponent<Text>();
+
+		while (numbers > 0)
+		{
+			counterLabel.text = numbers.ToString();
+			yield return new WaitForSeconds(seconds);
+			numbers--;
+		}
+
+		counterLabel.text = "Go!";
+		yield return new WaitForSeconds(2);
+		counterLabel.text = "";
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
