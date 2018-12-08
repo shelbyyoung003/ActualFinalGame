e6f09e2974e2c00459c174c3464b0783618b49d1
Shelby <shelbyyoung003@gmail.com>
2018-12-07 22:52:11 -0700

    Destroy Arrows

---
+++ FinalGame/Assets/Scripts/DestroyArrows.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyArrows : MonoBehaviour {
+
+	IEnumerator Start ()
+	{
+
+		yield return new WaitForSeconds(2);
+		Destroy(gameObject);
+
+	}
+
+}
