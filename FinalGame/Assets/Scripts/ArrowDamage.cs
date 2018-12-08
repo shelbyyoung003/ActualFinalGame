c1f71ad1ae964cce2b75aab4a06669ad269bf2ee
Shelby <shelbyyoung003@gmail.com>
2018-12-08 00:46:20 -0700

    Arrow Damage

---
+++ FinalGame/Assets/Scripts/ArrowDamage.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowDamage : MonoBehaviour {
+
+	private void OnCollisionEnter(Collision other)
+	{
+		Destroy(gameObject);
+		Destroy(other.gameObject);
+	}
+}
